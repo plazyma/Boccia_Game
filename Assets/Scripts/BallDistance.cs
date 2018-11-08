c5947c5799ecc59246b3bcf33c7713f1e7fc5da2
nikodems <32880727+nikodems@users.noreply.github.com>
2018-11-08 09:18:45 +0000

    Test

---
+++ Assets/Scripts/BallDistance.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDistance : MonoBehaviour {
+
+    //Declare list of balls
+    public List<GameObject> ballList = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        //Loop through all objects with tag "Ball" and add them to the list
+        foreach (GameObject ball in GameObject.FindGameObjectsWithTag("Ball"))
+        {
+            ballList.Add(ball);
+        }
+    }
+   
+	// Update is called once per frame
+	void Update () {
+        FindClosestBall();
+    }
+
+    //Find the closest ball to the jack
+    void FindClosestBall()
+    {
+        //Declaring variables
+        float shortest = 10;
+        GameObject closest = gameObject;
+
+        //Iterate through list
+        foreach (GameObject ball in ballList)
+        {
+            //Calculate distance between current ball and the jack
+            float distance = Vector3.Distance(ball.transform.position, transform.position);
+
+            //If the current ball's distance is lower 
+            if (distance < shortest)
+            {
+                //This is the closest ball
+                shortest = distance;
+                closest = ball;
+            }
+
+            //DEBUG
+          //  Debug.Log("Shortest distance: " + closest.ToString() + shortest);
+
+        }
+    }
+}
+
+
+//DIFFERENTIATING BETWEEN TEAMS
+//MULTIPLE TAGS IN ONE TAG
+//USE PARENT/CHILD OBJECTS
---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throw : MonoBehaviour {
+    //Initializing
+    public float power;
+    public Rigidbody rb;
+    public GameObject arrow;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        power = 0;
+
+        transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+
+        arrow.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //while space is pressed increase power
+        //TODO - Change how fast power is increased
+        if (Input.GetKey("space"))
+        {
+            arrow.SetActive(false);
+            while (power < 10)
+            {
+                power += 0.1f;
+
+                print(power);
+            }
+        }
+        //Apply force on release
+        if(Input.GetKeyUp("space"))
+        {
+            //Apply force to ball
+            //Z - Force based on rotation
+            //TODO - Update how z force is calculated
+            rb.AddForce(power, 0, -power * (transform.rotation.y), ForceMode.Impulse);
+            rb.useGravity = true;
+        }
+
+        //When right is pressed rotate ball
+        if(Input.GetKeyDown("right"))
+        {
+            //Restrict how far user can rotate
+           if (transform.rotation.eulerAngles.y < 90.0f || transform.rotation.eulerAngles.y > 250.0f)
+            {
+                //Show "arrow" to indicate where ball is pointing
+                arrow.SetActive(true);
+                transform.Rotate(0.0f, 10.0f, 0.0f);
+                //DEBUG
+                print(transform.eulerAngles.y);
+            }
+        }
+        //When left is pressed rotate in opposite direction
+        if(Input.GetKeyDown("left"))
+        {
+            //Restrict how far user can rotate
+            if (transform.rotation.eulerAngles.y < 120.0f || transform.rotation.eulerAngles.y > 270.0f)
+            {
+                //Show arrow to indicate where ball is pointing
+                arrow.SetActive(true);
+                transform.Rotate(0.0f, -10.0f, 0.0f);
+                //DEBUG
+                print(transform.eulerAngles.y);
+            }
+        }
+    }
+}
