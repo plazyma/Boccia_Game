0440c3e64f8bec8bf4c71434195941a5919fe527
nikodems <32880727+nikodems@users.noreply.github.com>
2018-11-29 16:28:46 +0000

    Added sound when clicking button

--- Assets/Scripts/StartScript.cs
+++ Assets/Scripts/StartScript.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class StartScript : MonoBehaviour {
 
+    public AudioSource audioSource;
+    public AudioClip buttonClick;
+
     public void Onclick()
     {
+        audioSource.clip = buttonClick;
+        audioSource.Play();
         // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
         SceneManager.LoadScene("Game", LoadSceneMode.Single);
     }
