8643e256280f8b64a0a5ef39f4c6a22cfb7ddb8c
Nikodem <nikodems100@gmail.com>
2019-10-20 14:41:34 +0100

    Menu Fixes
    Fixed keyboard/controller navigation with credits screen

--- Assets/Scripts/MenuScripts/CreditsNavigation.cs
+++ Assets/Scripts/MenuScripts/CreditsNavigation.cs
@@ -1,56 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class MenuNavigation : MonoBehaviour {
+public class CreditsNavigation : MonoBehaviour
+{
 
-    //neighbouring buttons
     public GameObject up, down;
-    public GameObject quick, start, settings, credits, exit;
+    public GameObject back;
 
     public GameObject selectedObject;
 
     public AudioSource audioSource;
 
-    //single movement
     bool dPadPressed = false;
 
     // Use this for initialization
-    void Start () {
-
+    void Start()
+    {
         //set default position over quick play
         if (name == "Selector")
         {
-            selectedObject = quick;
-            transform.position = quick.transform.position;
+            selectedObject = back;
+            transform.position = back.transform.position;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (name == "Selector")
         {
+
             moveButton();
         }
-	}
-
+    }
 
     void moveButton()
     {
         if ((Input.GetKeyDown("up") || Input.GetAxis("DPadY") == 1) && !dPadPressed)
         {
-            selectedObject = selectedObject.GetComponent<MenuNavigation>().up;
+            selectedObject = selectedObject.GetComponent<CreditsNavigation>().up;
 
             transform.position = new Vector3(selectedObject.transform.position.x, selectedObject.transform.position.y, selectedObject.transform.position.z - 1);
 
             dPadPressed = true;
-
         }
         if ((Input.GetKeyDown("down") || Input.GetAxis("DPadY") == -1) && !dPadPressed)
         {
-            selectedObject = selectedObject.GetComponent<MenuNavigation>().down;
+            selectedObject = selectedObject.GetComponent<CreditsNavigation>().down;
 
             transform.position = new Vector3(selectedObject.transform.position.x, selectedObject.transform.position.y, selectedObject.transform.position.z - 1);
 
@@ -66,36 +64,15 @@ public class MenuNavigation : MonoBehaviour {
 
         if (Input.GetKeyDown("space") || Input.GetButtonDown("A"))
         {
-            if (selectedObject == quick)
+            if (selectedObject == back)
             {
-                //jump into game
-                quick.GetComponent<StartScript>().QuickPlay();
-            }
-            else if (selectedObject == start)
-            {
-                //goto player selection
-                start.GetComponent<StartScript>().Onclick();
-            }
-            else if (selectedObject == settings)
-            {
-                //goto player selection
-                settings.GetComponent<SettingScript>().gotoSettings();
-            }
-            else if (selectedObject == credits)
-            {
-                credits.GetComponent<CreditsScript>().GoToCredits();
-
-            }
-            else if (selectedObject == exit)
-            {
-                //goto player selection
-                exit.GetComponent<QuitScript>().doExitGame();
-                //confirm sound
-                audioSource.GetComponent<PlaySound>().playSound();
-                
+                //goto game settings
+                back.GetComponent<CreditsScript>().ReturnToMainMenu();
             }
 
+            audioSource.GetComponent<PlaySound>().playSound();
         }
+
         if (Input.GetAxis("MouseX") != 0 || Input.GetAxis("MouseY") != 0)
         {
             //move it off screen
